ca0e7881da972b35bf6d0534198f61c7d211d773
Arthur Fernandes <166949523+arthurfer27@users.noreply.github.com>
2025-03-19 08:12:44 -0300

    Adiciona novas rotas (POST, PUT, DELETE)

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-  "github.com/labstack/echo/v4"
-  "github.com/labstack/echo/v4/middleware"
-  "log/slog"
-  "net/http"
-  "errors"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -18,7 +20,10 @@ func main() {
 
   // Routes
   e.GET("/students", getStudents)
-  e.GET("/teachers", getTeachers)
+  e.POST("/students", createStudents)
+  e.GET("/students/:id", getStudent)
+  e.PUT("/students/:id", updateStudent)
+  e.DELETE("/students/:id", deleteStudent)
 
   // Start server
   if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -31,6 +36,24 @@ func getStudents(c echo.Context) error {
   return c.String(http.StatusOK, "List of all students")
 }
 
-func getTeachers(c echo.Context) error {
-	return c.String(http.StatusOK, "List of all teachers")
+func createStudents(c echo.Context) error {
+	return c.String(http.StatusOK, "Create student")
+  }
+
+  func getStudent(c echo.Context) error {
+	id := c.Param("id")
+	getStud := fmt.Sprintf("Get %s student", id)
+	return c.String(http.StatusOK, getStud)
+  }
+
+  func updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	updateStud := fmt.Sprintf("Update %s student", id)
+	return c.String(http.StatusOK, updateStud)
+  }
+
+  func deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	deleteStud := fmt.Sprintf("Delete %s student", id)
+	return c.String(http.StatusOK, deleteStud)
   }
